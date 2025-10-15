d4ee587b196705d949bb1fa4a1ae0b859464b968
Paul8710 <paulletellierpro@gmail.com>
2025-10-15 12:00:52 +0200

    first api

--- src/server.js
+++ src/server.js
@@ -1,11 +1,30 @@
-import http from 'http'
+import express from 'express'
 
 const port = process.env.PORT || 3000;
-const server = http.createServer((req, res) => {
-    res.end('HELLO !')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/questions', (req, res) =>{
+    res.status(200).send([
+        {
+            id: 1,
+            question: "Quelle est la capitale de la France ?",
+            answer: 'Paris',
+        }
+    ])
 })
 
-server.listen(port, () =>{
+app.post('/questions', (req, res) => {
+    const {question, answer} = req.body
+    if (!question || !answer){
+        return res.status(400).send({ error: 'Invalid request'})
+    }
+    res.status(201).send({ message: 'Question Created'})
+})
+
+app.listen(port, () =>{
     console.log(`Server is running on http://localhost:${port}`)
 })
 
