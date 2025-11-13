650267caff6bd6cdc7e13eb6d5467d2a2af04b47
Paul8710 <paulletellierpro@gmail.com>
2025-11-13 17:53:56 +0100

    bug fixe

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -12,15 +12,15 @@ import {request, response} from 'express'
  */
 export const createUsers = async (req, res) => {
     const {email, username, password} = req.body
-    console.log(email, username, password)
+    //console.log(email, username, password)
     if (!email || !username || !password){
         return res.status(400).send({ error: 'Invalid request'})
     }
     
     try {
         let hashpassword = bcrypt.hashSync(password,12)
-        const questions = await db.insert(usersTable).values({email: email, username: username, password: hashpassword}).returning()
-        res.status(200).json(questions)
+        const questions = await db.insert(usersTable).values({email: email, username: username, password: hashpassword}).returning({email: usersTable.email, username: usersTable.username, id: usersTable.id})
+        res.status(200).json({message:'Good', userData: questions, token: 'TOKEN'})
     } catch (error) {
         console.error(error)
         res.status(500).json({
